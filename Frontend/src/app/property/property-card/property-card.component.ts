a7581ef1970ed2d8eff6947ca662b86b1797e16f
ElizabethJoseph25 <elizabeth.joseph@simelabs.com>
2021-12-30 16:55:42 +0530

    update

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
@@ -10,12 +10,8 @@ import { Component } from "@angular/core";
 
 })
 export class PropertyCardComponent {
-  Property: any = {
-    "Id" : 1,
-    "Type" : "House",
-    "Name" : "Birla House",
-    "Price" : 12000
-
+  @Input() property : any
 
   }
-}
+
+
